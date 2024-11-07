b216a07f971ef78d7c6fab4a903c914900a8a2e2
Dauchy Esteban <esteban_dauchy@ens.univ-artois.fr>
2024-11-07 15:56:56 +0100

    Fin dev

--- src/app/page.js
+++ src/app/page.js
@@ -1,79 +1,38 @@
 const ArrayProjets = [
     {
-        nom : "Projet 1",
-        description : "Décrire le projet",
-        technos: ["tec1", "tec2", "tec3"]
+        nom : "Poké WG2D",
+        description : "Un pokédex réalisé en Flask.py, HTML5 et CSS3 grâce à l'API Tyradex"
     },
     {
-        nom : "Projet 1",
-        description : "Décrire le projet",
-        technos: ["tec1", "tec2", "tec3"]
+        nom : "Le Panthéon",
+        description : "Site web en PHP, Laravel, Blade permettant de créer et consulter des histoires dont vous êtes le héros !"
     },
     {
-        nom : "Projet 1",
-        description : "Décrire le projet",
-        technos: ["tec1", "tec2", "tec3"]
+        nom : "FlatCraft",
+        description : "Un jeu vidéo 2D réalisé en Java reprenant le style graphique et la manière de jouer de minecraft"
     },
     {
-        nom : "Projet 1",
-        description : "Décrire le projet",
-        technos: ["tec1", "tec2", "tec3"]
+        nom : "J-O 2024",
+        description : "Création d'un site web en PHP, Laravel, Blade regroupant les différents évènements et sportifs lors des Jeux Olympique de 2024"
     },
     {
-        nom : "Projet 1",
-        description : "Décrire le projet",
-        technos: ["tec1", "tec2", "tec3"]
+        nom : "Ray-tracing App",
+        description : "Application de lanceur de rayon (rendu 3D) réalisé en Java"
     },
     {
-        nom : "Projet 1",
-        description : "Décrire le projet",
-        technos: ["tec1", "tec2", "tec3"]
+        nom : "Fractals Creator",
+        description : "Application créée en Java permettant de générer différentes fractales selon les modèles les plus connus  ",
     },
     {
-        nom : "Projet 1",
-        description : "Décrire le projet",
-        technos: ["tec1", "tec2", "tec3"]
+        nom : "Application Météo",
+        description : "Réalisation d'une application météo avec Flask.py en utilisant l'API wttr.in",
     },
     {
-        nom : "Projet 1",
-        description : "Décrire le projet",
-        technos: ["tec1", "tec2", "tec3"]
+        nom : "Guide des jeux",
+        description : "En cours de création d'un site web permettant de regrouper tous les jeux existants"
 
-    },
-    {
-        nom : "Projet 1",
-        description : "Décrire le projet",
-        technos: ["tec1", "tec2", "tec3"]
-    },
+    }
 ];
-
-const ArrayExperiences = [
-    {
-        poste : "Nom du poste",
-        Description : "Quel était mon role",
-        Dates : [21-11-24, 22-11-24]
-    },
-    {
-        poste : "Nom du poste",
-        Description : "Quel était mon role",
-        Dates : [21-11-24, 22-11-24]
-    },
-    {
-        poste : "Nom du poste",
-        Description : "Quel était mon role",
-        Dates : [21-11-24, 22-11-24]
-    },
-    {
-        poste : "Nom du poste",
-        Description : "Quel était mon role",
-        Dates : [21-11-24, 22-11-24]
-    },
-    {
-        poste : "Nom du poste",
-        Description : "Quel était mon role",
-        Dates : [21-11-24, 22-11-24]
-    },
-]
 
 const ArraySocial = [
     {
@@ -92,7 +51,7 @@ const ArraySocial = [
         lien : "mailto:estebandauchy@gmail.com",
         nom : "Email"
     }
-]
+];
 
 const ArrayCompetences = [
     {
@@ -140,6 +99,7 @@ const ArrayCompetences = [
 export default function Home() {
     return (
         <div>
+            <link rel="icon" href="../../images/Moi.webp" type="image/x-icon"/>
             <div className="Intro">
                 <div className="text-content">
                     <h3>Bienvenue sur mon portfolio !</h3>
@@ -157,7 +117,7 @@ export default function Home() {
                 <div className="box-container">
                     {ArraySocial.map((valeur, index) => (
                         <a key={index} className="boxSocial" href={valeur.lien} target="_blank">
-                            <h3>{valeur.nom}</h3>
+                            <h4>{valeur.nom}</h4>
                         </a>
                     ))}
                 </div>
@@ -167,21 +127,30 @@ export default function Home() {
                     <h3>A propos de <span>moi</span></h3>
                     <p>Je suis actuellement en troisième année de BUT informatique avec le parcours développement
                         d'application
-                        à l'université de Lens (62300). Mes envies de m'orienter vers l'informatique ont commencé en
+                        à <span>l'université de Lens (62300)</span>. Mes envies de m'orienter vers l'informatique ont commencé en
                         première au
-                        lycée André Malraux de Béthune (62400) lorsque j'ai sélectionné l'option Numérique et Sciences
+                        <span>lycée André Malraux de Béthune (62400)</span> lorsque j'ai sélectionné l'option Numérique et Sciences
                         de l'informatique
                         (NSI) en plus des options Sciences de l'Ingénieur et Mathématiques dans un Bac général option
                         Euro Anglais.
                     </p>
-                    <p>Au fil des années passées à développer et à comprendre des systèmes d'informations complexes, grâce à la qualité des enseignements de mon
-                        université, j'ai pu obtenir une alternance au sein de CGI Lille, une société de prestation de service canadienne qui opère dans le monde entier.
-                        Dans cette entreprise pour laquelle je suis alternant, j'occupe actuellement le poste de DevOPS et développeur junior, ce qui me permet de maîtriser plus d'outils, de technologies, de langages, et bien plus encore...
+                    <p>Au fil des années passées à développer et à comprendre des systèmes d'informations complexes,
+                        grâce à la qualité des enseignements de mon
+                        université, j'ai pu obtenir une alternance au sein de <span>CGI Lille</span>, une société de prestation de
+                        service canadienne qui opère dans le monde entier.
+                        Dans cette entreprise pour laquelle je suis <span>alternant</span>, j'occupe actuellement le poste de <span>DevOPS</span>
+                         et <span>développeur junior</span>, ce qui me permet de maîtriser plus d'outils, de technologies, de
+                        langages, et bien plus encore...
                     </p>
                     <p>
-                        En dehors de mon travail, j'aime beaucoup le sport et plus particulièrement le volley au club de <a href="https://www.facebook.com/groups/volleycuinchy?locale=fr_FR" target="_blank">Cuinchy Volleyball</a> pour lequel je suis joueur mais également membre de la direction en tant que responsable de la communication numérique.
-                        Je suis également passionné par la musique en tout genre, tout type et toute époque, ayant tout de même une légère préférence pour le Rock et le Metal.
-                        J'aime également sortir avec des amis, rencontrer de nouvelles personnes, et profiter lorsque j'ai du temps libre avec des personnes qui me sont chères.
+                        En dehors de mon travail, j'aime beaucoup le sport et plus particulièrement le volley au club
+                        de <span><a href="https://www.facebook.com/groups/volleycuinchy?locale=fr_FR" target="_blank">Cuinchy
+                        Volleyball</a></span> pour lequel je suis joueur mais également membre de la direction en tant que
+                        responsable de la communication numérique.
+                        Je suis également passionné par la musique en tout genre, tout type et toute époque, ayant tout
+                        de même une légère préférence pour le Rock et le Metal.
+                        J'aime également sortir avec des amis, rencontrer de nouvelles personnes, et profiter lorsque
+                        j'ai du temps libre avec des personnes qui me sont chères.
                     </p>
                 </div>
                 <div className="image-container">
@@ -227,11 +196,6 @@ export default function Home() {
                         <div key={index} className="box">
                             <h1>{projet.nom}</h1>
                             <p>{projet.description}</p>
-                            <ul>
-                                {projet.technos.map((techno, techIndex) => (
-                                    <li key={techIndex}>{techno}</li>
-                                ))}
-                            </ul>
                         </div>
                     ))}
                 </div>
